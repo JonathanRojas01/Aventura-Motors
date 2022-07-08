c41eb634785eddf1ba3879a486ec57684bf1fe7e
Jonathan <101790501+JonathanRojas01@users.noreply.github.com>
2022-07-08 01:48:07 -0400

    Creada pagina de contacto

--- resources/views/public/page-contacto.blade.php
+++ resources/views/public/page-contacto.blade.php
@@ -1,6 +1,7 @@
 <div id="page-contacto">
 
     @include('public.template-parts.header')
+    @include('public.template-parts.seccion-contacto')
     @include('public.template-parts.footer')
 
 </div>
