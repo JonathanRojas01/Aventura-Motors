3f8edb07778b07bc7a1ac303a0b66d8711db7758
Jonathan <101790501+JonathanRojas01@users.noreply.github.com>
2022-07-08 12:15:02 -0400

    Terminada caja de ubicaciones

--- resources/views/public/template-parts/ubicacion.blade.php
+++ resources/views/public/template-parts/ubicacion.blade.php
@@ -6,39 +6,68 @@
 
         <div class="row">
 
-            <div class="col-7">
+            <div class="col-7" style="padding: 20px;">
                 <div class="row">
                     <div class="col-6">
-                        <table class="blue">
+                        <table class="blue f-12">
                             <tr>
-                                <th></th>
+                                <th class="lightblue" colspan="2">Información</th>
                             </tr>
                             <tr>
-                                <td class="fw-semibold">Dirección</td>
+                                <td>Dirección</td>
                                 <td>: Camino Chicureo 2.2, Colina</td>
                             </tr>
                             <tr>
-                                <td class="fw-semibold">Dirección</td>
-                                <td>: Camino Chicureo 2.2, Colina</td>
+                                <td>Jefe de sucursal</td>
+                                <td>: Freddy Cancino</td>
                             </tr>
                             <tr>
-                                <td class="fw-semibold">Dirección</td>
-                                <td>: Camino Chicureo 2.2, Colina</td>
+                                <td>Correo</td>
+                                <td>: fcancino@aventuramotors.cl</td>
                             </tr>
                             <tr>
-                                <td class="fw-semibold">Dirección</td>
-                                <td>: Camino Chicureo 2.2, Colina</td>
+                                <td>WhatsApp</td>
+                                <td>: +56 22712 3189</td>
                             </tr>
                             <tr>
-                                <td class="fw-semibold">Dirección</td>
-                                <td>: Camino Chicureo 2.2, Colina</td>
+                                <td>Horario sucursal</td>
+                                <td>: Lun a Vie de 09:00 a 19:00 hrs. <br> <span>&nbsp</span> Sáb 10:30 a 14:00 hrs.</td>
                             </tr>
                         </table>
                     </div>
-                    <div class="col-6"></div>
+                    <div class="col-6">
+                        <table class="blue f-12">
+                            <tr>
+                                <th class="lightblue" colspan="2">Contacto</th>
+                            </tr>
+                            <tr>
+                                <td>Ariel Valdivia</td>
+                                <td>: avaldivia@aventuramotors.cl</td>
+                            </tr>
+                            <tr>
+                                <td>Margarita Parada</td>
+                                <td>: mparada@aventuramotors.cl</td>
+                            </tr>
+                            <tr>
+                                <td>Victor Salazar</td>
+                                <td>: vsalazar@aventuramotors.cl</td>
+                            </tr>
+                            <tr>
+                                <td>Zorymar Medina</td>
+                                <td>: zmedina@aventuramotors.cl</td>
+                            </tr>
+                            <tr>
+                                <td>Escríbenos por WhatsApp <br>y te asesoramos</td>
+                                <td>: +569 3426 7958</td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
             </div>
-            <div class="col-5"></div>
+
+            <div class="col-5 p-0">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1006.0012622646734!2d-70.67696589671914!3d-33.28589173193039!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662b9132b297a31%3A0xc182fefdb83245b3!2sAventura%20Motors%20Chicureo!5e0!3m2!1ses!2scl!4v1657295546805!5m2!1ses!2scl" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
 
         </div>
 
