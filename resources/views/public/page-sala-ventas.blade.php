b4e914dada6f56056a39019f1f57167eadb768dd
Jonathan <101790501+JonathanRojas01@users.noreply.github.com>
2022-07-11 11:00:49 -0400

    Cajas de ubicación listas

--- resources/views/public/page-sala-ventas.blade.php
+++ resources/views/public/page-sala-ventas.blade.php
@@ -1,9 +1,8 @@
 <div id="page-sala-ventas">
 
     @include('public.template-parts.header')
-
+    @include('public.template-parts.banner-2')
     @include('public.template-parts.seccion-ubicaciones')
-
     @include('public.template-parts.footer')
 
 </div>
---
+++ resources/views/public/template-parts/banner-2.blade.php
@@ -0,0 +1,54 @@
+<div class="banner banner-2 d-flex flex-column px-5 zoom-in-out" style="background-image: url('{{ asset('assets/img/banner-sala-ventas.png') }}');">
+
+    <div class="content">
+        <h2 class="text-white f-60">SALAS <br> DE VENTAS</h2>
+        <p class="bgblue text-white">Visita nuestro Showroom y conoce tu próximo auto</p>
+    </div>
+
+
+    <div class="container-marcas">
+    
+        <div class="box-marca" data-aos="flip-left">
+            <img src="{{ asset('assets/img/marca1.svg') }}">
+        </div>
+
+        <div class="box-marca" data-aos="flip-left">
+            <img src="{{ asset('assets/img/marca2.svg') }}">
+        </div>
+
+        <div class="box-marca" data-aos="flip-left">
+            <img src="{{ asset('assets/img/marca3.svg') }}">
+        </div>
+
+        <div class="box-marca" data-aos="flip-left">
+            <img src="{{ asset('assets/img/marca4.svg') }}">
+        </div>
+
+        <div class="box-marca" data-aos="flip-left">
+            <img src="{{ asset('assets/img/marca5.svg') }}">
+        </div>
+
+        <div class="box-marca" data-aos="flip-left">
+            <img src="{{ asset('assets/img/marca6.svg') }}">
+        </div>
+
+        <div class="box-marca" data-aos="flip-left">
+            <img src="{{ asset('assets/img/marca7.svg') }}">
+        </div>
+
+        <div class="box-marca" data-aos="flip-left">
+            <img src="{{ asset('assets/img/marca8.svg') }}">
+        </div>
+
+        <div class="box-marca" data-aos="flip-left">
+            <img src="{{ asset('assets/img/marca9.svg') }}">
+        </div>
+
+        <div class="box-marca" data-aos="flip-left">
+            <img src="{{ asset('assets/img/marca10.svg') }}">
+        </div>
+
+    </div>
+
+
+</div>
